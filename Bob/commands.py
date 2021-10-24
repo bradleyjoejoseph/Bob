94c8acb39c3cf0c9de86164f61f70762d1aff7d5
Bradley Joseph <38166212+bradleyjoe007@users.noreply.github.com>
2021-10-24 17:06:16 +0100

    Added "/chrome" and fixed "/version"
    "/chrome" opens up the web browser chrome (trying to find an easier way for this), "/version" was not included in the 'cmds' variable and so did not work has been fixed.

--- Bob/commands.py
+++ Bob/commands.py
@@ -1,8 +1,9 @@
 #commands - Everything to do with commands
 import timer
+import os
 import versionControl
 
-cmds = ["/cmds", "/quit", "/timer", "/path"]
+cmds = ["/cmds", "/quit", "/timer", "/chrome", "/path", "/version"]
 
 def cmdExec(cmd):
     if cmd == "/cmds":
@@ -14,8 +15,9 @@ def cmdExec(cmd):
         m = int(input("Minutes: "))
         s = int(input("Seconds: "))
         timer.timer(h, m, s)
+    if cmd == "/chrome":
+        os.startfile("C:\Program Files\Google\Chrome\Application\Chrome")
     if cmd == "/path":
         print(versionControl.path)
     if cmd == "/version":
         print(versionControl.getVersion())
-    
